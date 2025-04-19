18b2d36a6ac33ed1adbf92cd420b26b53af3ff5a
bitDig6 <ummenabilajalal@gmail.com>
2025-04-19 20:05:23 +0600

    create api to fetch a single running campaign data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -35,6 +35,13 @@ async function run() {
     app.get('/runningCampaigns', async(req, res) => {
       const cursor = campaignsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/runningCampaigns/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = campaignsCollection.findOne(query);
       res.send(result);
     })
 
